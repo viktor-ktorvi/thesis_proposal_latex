1289e5f58bc4440753cd253ae05bf9d5ec776435
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-08-30 22:36:58 +0200

    add the metrics for the objective funtion

--- src/methodology/metrics/metrics.py
+++ src/methodology/metrics/metrics.py
@@ -37,10 +37,12 @@ def create_metrics():
     metrics = Subsection("Metrics", numbering=False)
     metrics.append(Command("input", "methodology/metrics/metrics_with_regards_to"))
 
+    header = ["Name", "Symbol", "Math expression", "Variables", "Unit"]
+
     equality_constraints = Subsubsection("Equality constrains", numbering=False)
     equality_constraints.append(
         create_metrics_table(
-            header=["Name", "Symbol", "Math expression", "Variables", "Unit"],
+            header=header,
             entries=[
                 TableEntry(name="absolute error",
                            symbol=math(rf"\Delta_{{abs}} {math_var}"),
@@ -62,7 +64,7 @@ def create_metrics():
     inequality_constraints = Subsubsection("Inequality constrains", numbering=False)
     inequality_constraints.append(
         create_metrics_table(
-            header=["Name", "Symbol", "Math expression", "Variables", "Unit"],
+            header=header,
             entries=[
                 TableEntry(name="upper bound error",
                            symbol=math(rf"\Delta_{{max}} {math_var}"),
@@ -81,6 +83,26 @@ def create_metrics():
         )
     )
     metrics.append(inequality_constraints)
+
+    objective_function = Subsubsection("Objective function", numbering=False)
+    objective_function.append(
+        create_metrics_table(
+            header=["Name", "Symbol", "Math expression", "Unit"],
+            entries=[
+                TableEntry(name="cost",
+                           symbol="C",
+                           expression=math(r"\objectivefunction"),
+                           unit=math(frac("currency", "h"))),
+                TableEntry(name="relative cost",
+                           symbol=math(r"C^{rel}"),
+                           expression=math(frac(r"\objectivefunction", r"C_{target}")),
+                           unit=math(r"\cdot"))
+            ],
+            caption="Metrics with regard to the objective function",
+            marker=Marker("objectivefunctionmetrics", prefix="tab")
+        )
+    )
+    metrics.append(objective_function)
 
     with open(os.path.join(current_directory, "metrics.tex"), "w") as f:
         f.write(metrics.dumps())
--- src/methodology/metrics/metrics_table.py
+++ src/methodology/metrics/metrics_table.py
@@ -6,11 +6,11 @@ from pylatex import Table, Tabular, NoEscape, Label, Marker, Command
 
 @dataclass
 class TableEntry:
-    name: str
-    symbol: str
-    expression: str
-    variables: str
-    unit: str
+    name: str = None
+    symbol: str = None
+    expression: str = None
+    variables: str = None
+    unit: str = None
 
 
 def create_metrics_table(header: List[str], entries: List[TableEntry], caption: str, marker: Marker):
@@ -20,7 +20,7 @@ def create_metrics_table(header: List[str], entries: List[TableEntry], caption: 
     metric_tabular.add_hline()
 
     for entry in entries:
-        fields = [NoEscape(field) for field in [entry.name, entry.symbol, entry.expression, entry.variables, entry.unit]]
+        fields = [NoEscape(field) for field in [entry.name, entry.symbol, entry.expression, entry.variables, entry.unit] if field is not None]
         metric_tabular.add_row(*fields)
         metric_tabular.add_hline()
 
