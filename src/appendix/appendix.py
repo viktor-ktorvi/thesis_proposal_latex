f2ed443ce051c4570b794a9c972eedb547baa01f
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-09-09 15:39:41 +0200

    add model selection

--- src/appendix/appendix.py
+++ src/appendix/appendix.py
@@ -8,10 +8,10 @@ def create_appendix():
     current_directory = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
     appendix = Chapter("Appendix", numbering=False, label="appendix")
-    appendix.append("TODO what dataset, how much, how many epochs, what jk, what grid search etc.")
     appendix.append(Command("input", "appendix/dataset"))
-    appendix.append(Command("input", "appendix/models"))
+    appendix.append(Command("input", "appendix/model_workflow"))
     appendix.append(Command("input", "appendix/model_diagram"))
+    appendix.append(Command("input", "appendix/model_selection"))
 
     with open(os.path.join(current_directory, "appendix.tex"), "w") as f:
         f.write(appendix.dumps())
